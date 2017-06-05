63568df983dc4348ec2eeead8e484665e48de35d
Mehmet Fidanboylu <mehmetf@google.com>
2017-06-05 16:38:29 -0700

    Run build tests for example packages only if the package contains the necessary platform files

--- lib/src/build_examples_command.dart
+++ lib/src/build_examples_command.dart
@@ -27,23 +27,23 @@ class BuildExamplesCommand extends Command<Null> {
   @override
   Future<Null> run() async {
     final List<String> failingPackages = <String>[];
-    for (Directory example in _getExamplePackages(packagesDir)) {
-      final String packageName =
-          p.relative(example.path, from: packagesDir.path);
+    for (_ExamplePackage examplePackage in _getExamplePackages(packagesDir)) {
+      final Directory dir = examplePackage.packageDir;
+      final String packageName = p.relative(dir.path, from: packagesDir.path);
 
-      if (argResults['ipa']) {
+      if (argResults['ipa'] && examplePackage.hasiOS) {
         print('\nBUILDING IPA for $packageName');
         final int exitCode = await runAndStream(
-            'flutter', <String>['build', 'ios', '--no-codesign'], example);
+            'flutter', <String>['build', 'ios', '--no-codesign'], dir);
         if (exitCode != 0) {
           failingPackages.add('$packageName (ipa)');
         }
       }
 
-      if (argResults['apk']) {
+      if (argResults['apk'] && examplePackage.hasAndroid) {
         print('\nBUILDING APK for $packageName');
         final int exitCode =
-            await runAndStream('flutter', <String>['build', 'apk'], example);
+            await runAndStream('flutter', <String>['build', 'apk'], dir);
         if (exitCode != 0) {
           failingPackages.add('$packageName (apk)');
         }
@@ -63,13 +63,33 @@ class BuildExamplesCommand extends Command<Null> {
     print('All builds successful!');
   }
 
-  Iterable<Directory> _getExamplePackages(Directory dir) => dir
-          .listSync(recursive: true)
-          .where((FileSystemEntity entity) =>
-              entity is Directory && p.basename(entity.path) == 'example')
-          .where((FileSystemEntity entity) {
-        final Directory dir = entity;
-        return dir.listSync().any((FileSystemEntity entity) =>
-            entity is File && p.basename(entity.path) == 'pubspec.yaml');
-      });
+  Iterable<_ExamplePackage> _getExamplePackages(Directory dir) {
+    return dir
+        .listSync(recursive: true)
+        .where((FileSystemEntity entity) =>
+            entity is Directory && p.basename(entity.path) == 'example')
+        .where((FileSystemEntity entity) {
+      final Directory dir = entity;
+      return dir.listSync().any((FileSystemEntity entity) =>
+          entity is File && p.basename(entity.path) == 'pubspec.yaml');
+    }).map((FileSystemEntity entity) {
+      final Directory dir = entity;
+      final List<FileSystemEntity> contents = dir.listSync();
+      return new _ExamplePackage(
+        packageDir: dir,
+        hasiOS: contents.any((FileSystemEntity entity) =>
+            entity is Directory && p.basename(entity.path) == 'ios'),
+        hasAndroid: contents.any((FileSystemEntity entity) =>
+            entity is Directory && p.basename(entity.path) == 'android'),
+      );
+    });
+  }
+}
+
+class _ExamplePackage {
+  final Directory packageDir;
+  final bool hasiOS;
+  final bool hasAndroid;
+
+  _ExamplePackage({this.packageDir, this.hasiOS, this.hasAndroid});
 }

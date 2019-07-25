4c32294e2330cf1f99b814e1e2d72c1e56bbde8f
Maurice Parrish <bmparr@google.com>
2019-07-25 15:06:29 -0700

    Bug fixes and better output

--- lib/src/license_check_command.dart
+++ lib/src/license_check_command.dart
@@ -73,49 +73,49 @@ class LicenseCheckCommand extends PluginCommand {
     final int year = DateTime.now().year;
     return '// Copyright $year The $author Authors. All rights reserved.\n'
         '// Use of this source code is governed by a BSD-style license that can be\n'
-        '// found in the LICENSE file.\n\n';
+        '// found in the LICENSE file.\n';
   }
 
   static String _getLicenseFile(String author) {
     final int year = DateTime.now().year;
-    return '// Copyright $year The $author Authors. All rights reserved.'
-        '//'
-        '// Redistribution and use in source and binary forms, with or without'
-        '// modification, are permitted provided that the following conditions are'
-        '// met:'
-        '//'
-        '//    * Redistributions of source code must retain the above copyright'
-        '// notice, this list of conditions and the following disclaimer.'
-        '//    * Redistributions in binary form must reproduce the above'
-        '// copyright notice, this list of conditions and the following disclaimer'
-        '// in the documentation and/or other materials provided with the'
-        '// distribution.'
-        '//    * Neither the name of Google Inc. nor the names of its'
-        '// contributors may be used to endorse or promote products derived from'
-        '// this software without specific prior written permission.'
-        '//'
-        '// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS'
-        '// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT'
-        '// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR'
-        '// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT'
-        '// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,'
-        '// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT'
-        '// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,'
-        '// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY'
-        '// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT'
-        '// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE'
-        '// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.';
+    return '// Copyright $year The $author Authors. All rights reserved.\n'
+        '//\n'
+        '// Redistribution and use in source and binary forms, with or without\n'
+        '// modification, are permitted provided that the following conditions are\n'
+        '// met:\n'
+        '//\n'
+        '//    * Redistributions of source code must retain the above copyright\n'
+        '// notice, this list of conditions and the following disclaimer.\n'
+        '//    * Redistributions in binary form must reproduce the above\n'
+        '// copyright notice, this list of conditions and the following disclaimer\n'
+        '// in the documentation and/or other materials provided with the\n'
+        '// distribution.\n'
+        '//    * Neither the name of Google Inc. nor the names of its\n'
+        '// contributors may be used to endorse or promote products derived from\n'
+        '// this software without specific prior written permission.\n'
+        '//\n'
+        '// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS\n'
+        '// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT\n'
+        '// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR\n'
+        '// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT\n'
+        '// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,\n'
+        '// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT\n'
+        '// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n'
+        '// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n'
+        '// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n'
+        '// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n'
+        '// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n';
   }
 
   void _outputNoLicenseError(List<Directory> pluginDirs, String license) {
-    print('The following plugins don\'t contain a valid LICENSE file.');
+    print('The following plugins don\'t contain a valid LICENSE file.\n');
 
     for (Directory dir in pluginDirs) {
-      print(dir.path);
+      print(dir.path.split('/').last);
     }
 
     print(
-        'Please run `pub global run flutter_plugin_tools license-check --update`\n'
+        '\nPlease run `pub global run flutter_plugin_tools license-check --update`\n'
         'or add a LICENSE file containing: \n$license');
   }
 
@@ -127,12 +127,14 @@ class LicenseCheckCommand extends PluginCommand {
 
     print(
       'Please run `pub global run flutter_plugin_tools license-check --update`\n'
-      'or add the following license to the beginning of each file below: \n$license',
+      'or add the following license to the beginning of each file below: \n\n$license',
     );
 
     for (File file in files) {
       print(file.path);
     }
+
+    print('-' * 50);
   }
 
   void _updateLicenseHeaders(List<File> files, String license) {
@@ -160,9 +162,7 @@ class LicenseCheckCommand extends PluginCommand {
   }
 
   String _parseAuthor(String string) {
-    final List<String> validNames = <String>['Chromium', 'Flutter'];
-
-    for (String name in validNames) {
+    for (String name in _validAuthorNames) {
       if (string.contains(name)) {
         return name;
       }
@@ -176,6 +176,7 @@ class LicenseCheckCommand extends PluginCommand {
     checkSharding();
 
     final List<Directory> pluginsWithoutLicenseFile = <Directory>[];
+    bool fail = false;
     await for (Directory pluginDir in getPlugins()) {
       final List<File> pluginFiles = <File>[];
 
@@ -222,18 +223,19 @@ class LicenseCheckCommand extends PluginCommand {
       }
 
       if (filesWithoutValidHeader.isNotEmpty) {
+        fail = true;
         if (argResults['print']) {
           _outputInvalidLicenseHeaderError(
             pluginDir,
             filesWithoutValidHeader,
-            _getLicenseHeader('author'),
+            _getLicenseHeader(author),
           );
         }
 
         if (argResults['update']) {
           _updateLicenseHeaders(
             filesWithoutValidHeader,
-            _getLicenseHeader('author'),
+            _getLicenseHeader(author),
           );
         }
       }
@@ -253,6 +255,10 @@ class LicenseCheckCommand extends PluginCommand {
           licenseFile.writeAsStringSync(_getLicenseFile('Flutter'));
         }
       }
+    }
+
+    if (pluginsWithoutLicenseFile.isNotEmpty || fail) {
+      throw ToolExit(1);
     }
 
     print('All required files contain licenses!');

9461d71d75f55b14057bd33af870bdd5a0a4d69b
Ben Konyi <bkonyi@google.com>
2019-11-25 16:04:10 -0800

    Bump Firebase timeout

--- lib/src/firebase_test_lab_command.dart
+++ lib/src/firebase_test_lab_command.dart
@@ -180,7 +180,7 @@ class FirebaseTestLabCommand extends PluginCommand {
             '--test',
             'build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk',
             '--timeout',
-            '2m',
+            '5m',
             '--results-bucket=${argResults['results-bucket']}',
             '--results-dir=${argResults['results-dir']}',
           ];

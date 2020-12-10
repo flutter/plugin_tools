c7fa90cdd3ff0e0cf19fc130405963531916767b
Emmanuel Garcia <egarciad@google.com>
2020-12-10 15:57:50 -0800

    Entire federated dir

--- test/common_test.dart
+++ test/common_test.dart
@@ -61,6 +61,17 @@ void main() {
     ]);
     expect(plugins, unorderedEquals(<String>[plugin2.path]));
   });
+
+  test('exclude entire federated plugins when plugins flag is specified', () async {
+    createFakePlugin('plugin1', parentDirectoryName: 'federated');
+    final Directory plugin2 = createFakePlugin('plugin2');
+    await runner.run(<String>[
+      'sample',
+      '--plugins=federated/plugin1,plugin2',
+      '--exclude=federated'
+    ]);
+    expect(plugins, unorderedEquals(<String>[plugin2.path]));
+  });
 }
 
 class SamplePluginCommand extends PluginCommand {
